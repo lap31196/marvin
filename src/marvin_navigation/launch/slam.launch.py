854aa4393a86e72a9e7791e272fab1c34c643828
lap31196 <laandrad@espol.edu.ec>
2023-08-13 13:29:23 -0500

    Cambio de funciones

---
+++ src/marvin_navigation/launch/slam.launch.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch import LaunchContext
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import EnvironmentVariable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    gazebo_launch_path = PathJoinSubstitution(
+        [FindPackageShare('marvin_gazebo'), 'launch',
+         'gazebo.launch.py']
+    )
+
+    slam_launch_path = PathJoinSubstitution(
+        [FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py']
+    )
+
+    slam_config_path = PathJoinSubstitution(
+        [FindPackageShare('marvin_navigation'), 'config', 'slam.yaml']
+    )
+
+    navigation_launch_path = PathJoinSubstitution(
+        [FindPackageShare('nav2_bringup'), 'launch', 'navigation_launch.py']
+    )
+
+    nav2_config_path = PathJoinSubstitution(
+        [FindPackageShare('marvin_navigation'), 'config', 'navigation.yaml']
+    )
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gazebo_launch_path),
+            launch_arguments={'use_sim_time': 'false',
+                              'publish_joints': 'false',
+                              'rviz': 'false',
+                              'rviz2': 'true'
+                              }.items()
+        ),
+        DeclareLaunchArgument(
+            name='sim',
+            default_value='true',
+            description='Enable use_sime_time to true'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch_path),
+            launch_arguments={
+                'use_sim_time': LaunchConfiguration("sim"),
+                'slam_params_file': slam_config_path,
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(navigation_launch_path),
+            launch_arguments={
+                'use_sim_time': LaunchConfiguration("sim"),
+                'params_file': nav2_config_path
+            }.items()
+        )
+
+    ])
