92cd8a23cf5e51f2c81c3b0f0e7ff619e64a94ff
lap31196 <laandrad@espol.edu.ec>
2023-08-13 18:55:25 -0700

    commit

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo mkdir -p /etc/apt/keyrings
 curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
 sudo apt-get -y install apt-transport-https
-echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo lsb_release -cs main" | \
+echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
 sudo tee /etc/apt/sources.list.d/librealsense.list
 sudo apt-get update
 sudo apt-get -y install librealsense2*
