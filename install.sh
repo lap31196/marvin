269343d28ab5656e0f3468d963aadaa450564dbd
lap31196 <laandrad@espol.edu.ec>
2023-08-16 18:34:27 -0500

    sdsdds

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sfh
-sudo apt-get install apt-utils
+sudo apt-get -y install apt-utils
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u -y
 sudo apt-get -y install librealsense2-utils
