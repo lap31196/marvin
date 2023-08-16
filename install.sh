68c81727147fedce3d3471a134d97e6ab838d962
lap31196 <laandrad@espol.edu.ec>
2023-08-16 18:26:58 -0500

    j

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sfh
-sudo apt-get -y update
 sudo apt-get install apt-utils
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u -y
