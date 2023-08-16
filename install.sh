75f4a9fda0e440c05aad78035ed267c8121813d8
lap31196 <laandrad@espol.edu.ec>
2023-08-16 18:10:45 -0500

    j

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u -y
 sudo apt-get -y install librealsense2-utils
 sudo apt-get -y install librealsense2-dev
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' -y
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt -y install curl 
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - -y
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt -y install ros-foxy-realsense2-*
 
 sudo apt -y install ros-foxy-gazebo-ros-pkgs
