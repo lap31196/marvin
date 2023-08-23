4203916adb6ed1ae974adfdfa3a462a5a2608ef5
lap31196 <74328912+lap31196@users.noreply.github.com>
2023-08-22 20:57:19 -0500

    Delete marvin_ws/build directory

--- marvin_ws/build/openslam_gmapping/sensor/sensor_base/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marvin/marvin/marvin_ws/src/marvin_slam/openslam_gmapping/sensor/sensor_base
-# Build directory: /home/marvin/marvin/marvin_ws/build/openslam_gmapping/sensor/sensor_base
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- marvin_ws/build/openslam_gmapping/sensor/sensor_range/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marvin/marvin/marvin_ws/src/marvin_slam/openslam_gmapping/sensor/sensor_range
-# Build directory: /home/marvin/marvin/marvin_ws/build/openslam_gmapping/sensor/sensor_range
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
